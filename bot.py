44311d1744107965f86d977bc9851ae25676c555
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-09-06 18:15:51 +0300

    fixed bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
-import telebot
-from jinja2 import Template
 from os import getenv
 
-from models import catalog
+import telebot
+from jinja2 import Template
+
+from model import Meal
+
 
 TOKEN = getenv('BOT_TOKEN')
 if not TOKEN:
@@ -10,19 +12,24 @@ if not TOKEN:
 
 bot = telebot.TeleBot(TOKEN)
 
+
 with open('templates/catalog.md', 'r') as catalog_file:
     catalog_tmpl = Template(catalog_file.read())
 
+
 with open('templates/greetings.md', 'r') as greetings_file:
     greetings_tmpl = Template(greetings_file.read())
+
 
 @bot.message_handler(commands=['start'])
 def greet(message):
     bot.send_message(message.chat.id, greetings_tmpl.render())
 
+
 @bot.message_handler(commands=['menu'])
 def show_catalog(message):
-    bot.send_message(message.chat.id, catalog_tmpl.render(catalog=catalog), parse_mode='Markdown')
+    bot.send_message(message.chat.id, catalog_tmpl.render(catalog=Meal.query.all()), parse_mode='Markdown')
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
